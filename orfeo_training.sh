f42395e498c0dc8b20dbc59b464f9ce24e11d932
Gabrynho <masellagabriel@gmail.com>
2024-07-02 19:21:45 +0200

    funziona la prova quindi provo ad usare orfeo

--- orfeo_training.sh
+++ orfeo_training.sh
@@ -9,5 +9,4 @@
 #SBATCH --gpus=1
 
 source /u/dssc/gmasella/jupyter/bin/activate
-python3 -c "import torch as th; print(th.cuda.is_available())"
-#python3 c10_training_lr0.001.py
+python3 c10_training_lr0.001.py
